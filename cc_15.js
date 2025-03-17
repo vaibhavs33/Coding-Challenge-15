138c17617bbc0509bef5d437711c3259868daaff
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-03-16 20:31:42 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,5 @@
+//Task 1 - Base Structure Setup
+//When the document is fully loaded, a message will be logged to confirm
+document.addEventListener('DOMContentLoaded', function (){
+    console.log("Risk Dashboard Loaded");
+})
